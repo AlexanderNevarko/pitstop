94174093dd83855d2c162c1bcddea7ede3878071
Alexander <aleksandr.nevarko@frtk.ru>
2019-05-05 21:43:36 +0300

    App + Urls

---
+++ pitstop_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Map)
+admin.site.register(Simulation)
+admin.site.register(Sensor_data)
+admin.site.register(Real_time_data)
+admin.site.register(Predictions)
---
+++ pitstop_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Map(models.Model):
+    pass
+
+
+class Simulation(models.Model):
+    pass
+
+
+class Sensor_data(models.Model):
+    pass
+
+
+class Real_time_data(models.Model):
+    pass
+
+
+class Predictions(models.Model):
+    pass
