8aef3acaa36d7cf72ffc94e9f120abbab3a7e8ee
Alexander <aleksandr.nevarko@frtk.ru>
2019-05-21 19:37:13 +0300

    Update2

--- pitstop_app/views.py
+++ pitstop_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from random import randint
 
 def main_page(request):
     return render(request, 'pitstop_app/main_page.html', {})
@@ -7,17 +7,42 @@ def main_page(request):
 
 def jopa(request):
     if request.method == 'POST':
-        predict = get_prediction(request.POST)
-        return render(request, 'pitstop_app/prediction.html', predict)
+        #print(request.POST)
+        if validate(request.POST):
+            prediction = get_prediction(request.POST)
+            #print(prediction)
+            return render(request, 'pitstop_app/results.html', prediction)
+        else:
+            error_info = {"ERROR" : "Invalid data"}
+            return render(request, 'pitstop_app/404.html', error_info)
     else:
-        return render(request, 'pitstop_app/error.html', {})
+        return render(request, 'pitstop_app/404.html', {})
 
 
 def error(request):
-    return render(request, 'pitstop_app/error.html', {})
+    return render(request, 'pitstop_app/404.html', {})
 
 
 def get_prediction(data):
-    return {'predict' : 'Some prediction'}
+    value = 0.
+    value += 3. if data["weather"] == "rainy" else 2.
+    value += abs((float(data["temperature"]) - 21) / 2.)
+    value += len(data["car-model"]) / 10.
+    value += int(data["track-id"]) / 10.
+    value += randint(1, 5)
+    value = round(value)
+    
+    return {'prediction' : value}
+
+
+def validate(data):
+    validator = True
+    try:
+        float(data['temperature'])
+        int(data['track-id'])
+    except ValueError:
+        validator = False
+    return validator
+
 
 
